962ca4685754d6971e6c7ad7d0e6d99df3725e41
KaiGeller <gellerka@oregonstate.edu>
2022-04-20 21:26:57 -0700

    1

---
+++ fall_distance.py
@@ -0,0 +1,7 @@
+# Author : Kai Geller
+# GitHub Username : KaiGeller
+# Date : 4/20/2022
+# Description: Calculate the fall distance after being given the amount of time it falls
+dist = float(input("dist = fall distance:" ))
+t = (0.5*9.8*dist ** 2)
+print(str(t))
